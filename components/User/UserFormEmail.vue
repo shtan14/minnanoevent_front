18eaf725ec512a88c2c5cef1c4e2dafad0100e3d
shtan14 <wrzn.inw.snun@gmail.com>
2023-08-19 13:14:22 +0900

    フォームコンポーネント作成

---
+++ components/User/UserFormEmail.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-text-field
+    label="メールアドレスを入力"
+    placeholder="your@email.com"
+    outlined
+  />
+</template>
+
+<script>
+export default {};
+</script>
---
+++ components/User/UserFormPassword.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-text-field label="パスワードを入力" placeholder="8文字以上" outlined />
+</template>
+
+<script>
+export default {};
+</script>
