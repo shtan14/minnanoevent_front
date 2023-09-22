a9893bceb231e0872f165a4744f22e8fa3a2a5e2
shtan14 <wrzn.inw.snun@gmail.com>
2023-09-22 22:22:39 +0900

    login.vue パスワード再設定の表示を修正

--- pages/login.vue
+++ pages/login.vue
@@ -6,7 +6,7 @@
         <user-form-password :password.sync="params.auth.password" />
         <v-card-actions>
           <nuxt-link to="#" class="body-2 text-decoration-none">
-            パスワードを再設定
+            パスワードを再設定する
           </nuxt-link>
         </v-card-actions>
         <v-card-text class="px-0">
