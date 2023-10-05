5548b9356c80b73598ec49437c4f5514938215f9
shtan14 <wrzn.inw.snun@gmail.com>
2023-10-05 21:06:35 +0900

    topページにapiで取得したイベント情報を表示するようにeventlist.vueを修正

--- components/Event/EventList.vue
+++ components/Event/EventList.vue
@@ -3,7 +3,14 @@
     <h2>イベント一覧</h2>
     <ul>
       <li v-for="event in events" :key="event.id">
+        {{ event.id }}
         {{ event.title }}
+        {{ event.prefecture }}
+        {{ event.city }}
+        {{ event.location }}
+        {{ event.ticket_price }}
+        {{ event.event_start_datetime }}
+        {{ event.event_end_datetime }}
       </li>
     </ul>
   </div>
@@ -23,11 +30,11 @@ export default {
   methods: {
     // APIからイベントデータを取得するメソッド
     fetchEvents() {
-      // APIエンドポイントへのリクエストを送信し、データを取得
-      fetch("/api/v1/events")
-        .then((response) => response.json())
-        .then((data) => {
-          this.events = data; // 取得したデータをコンポーネントのデータにセット
+      // axiosを使用してAPIエンドポイントへのリクエストを送信し、データを取得
+      this.$axios
+        .get("/api/v1/events")
+        .then((response) => {
+          this.events = response.data; // 取得したデータをコンポーネントのデータにセット
         })
         .catch((error) => {
           console.error("イベントデータの取得に失敗しました", error);
