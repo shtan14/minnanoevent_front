1214ebba6dc198fea93380a5c99167e6278ad349
shtan14 <wrzn.inw.snun@gmail.com>
2023-10-09 10:39:46 +0900

    topページにv-cardを適用

--- components/Event/EventList.vue
+++ components/Event/EventList.vue
@@ -1,28 +1,29 @@
 <template>
   <div>
-    <h2>イベント一覧</h2>
-    <ul>
-      <li v-for="event in events" :key="event.id">
-        <p>{{ event.id }}</p>
-        <div v-if="event.event_images && event.event_images.length > 0">
-          <img
-            :src="event.event_images[0].event_image"
-            alt="アイキャッチ写真"
-            style="width: 100px; height: auto"
-          />
-        </div>
-        <p></p>
-        <p>{{ event.title }}</p>
-        <p>{{ event.description }}</p>
-        <p>{{ formatDatetime(event.event_start_datetime) }} から</p>
-        <p>{{ event.prefecture }} {{ event.city }} {{ event.location }}</p>
-        <span v-for="(category, index) in event.categories" :key="index">
+    <v-card v-for="event in events" :key="event.id" class="mb-4">
+      <v-img
+        v-if="event.event_images && event.event_images.length > 0"
+        :src="event.event_images[0].event_image"
+        alt="アイキャッチ写真"
+      ></v-img>
+      <v-card-title>{{ event.title }}</v-card-title>
+      <v-card-text>{{ event.description }}</v-card-text>
+      <v-card-text
+        >{{ formatDatetime(event.event_start_datetime) }} から</v-card-text
+      >
+      <v-card-text
+        >{{ event.prefecture }} {{ event.city }}
+        {{ event.location }}</v-card-text
+      >
+      <v-card-actions>
+        <v-btn
+          v-for="(category, index) in event.categories"
+          :key="index"
+        >
           ＃{{ category.category }}
-          <!-- 最後のカテゴリー以外にはカンマを追加 -->
-          <!-- <span v-if="index < event.categories.length - 1">, </span> -->
-        </span>
-      </li>
-    </ul>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
