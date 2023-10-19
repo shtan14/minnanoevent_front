f1f502d694a71ba2c58638ab3d9e47f5b6e8799b
shtan14 <wrzn.inw.snun@gmail.com>
2023-10-19 22:37:22 +0900

    eventdetail.vueファイルの作成

---
+++ pages/eventdetail.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <template v-if="isUserLoggedIn">
+      <logged-in-app-bar />
+    </template>
+    <template v-else>
+      <before-login-app-bar />
+    </template>
+    <v-main>
+      <nuxt />
+
+    </v-main>
+    <app-footer />
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "PagesEventDetail",
+  computed: {
+    isUserLoggedIn() {
+      const loggedIn = this.$auth.loggedIn(); // ログイン状態を呼び出し
+      console.log("ログイン状態", loggedIn);
+      return loggedIn;
+    },
+  },
+};
+</script>
