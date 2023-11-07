49e9b0265c4fbc8620766f0176d0f45079afea60
shtan14 <wrzn.inw.snun@gmail.com>
2023-11-07 23:51:02 +0900

    index.vue修正　検索機能のデータフロー改善

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <v-app>
     <template v-if="isUserLoggedIn">
-      <logged-in-app-bar />
+      <logged-in-app-bar @search="onSearch" />
     </template>
     <template v-else>
-      <before-login-app-bar />
+      <before-login-app-bar @search="onSearch" />
     </template>
     <v-main>
       <nuxt />
-      <event-list />
+      <event-list :search-term="searchTerm" />
     </v-main>
     <app-footer />
   </v-app>
@@ -17,11 +17,21 @@
 <script>
 export default {
   name: "PagesIndex",
+  data() {
+    return {
+      searchTerm: "",
+    };
+  },
   computed: {
     isUserLoggedIn() {
       const loggedIn = this.$auth.loggedIn(); // ログイン状態を呼び出し
       console.log("ログイン状態", loggedIn);
       return loggedIn;
+    },
+  },
+  methods: {
+    onSearch(searchTerm) {
+      this.searchTerm = searchTerm;
     },
   },
 };
