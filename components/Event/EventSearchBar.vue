aac66900a20b86d336c1e8b74db5251f5e2e40d3
shtan14 <wrzn.inw.snun@gmail.com>
2023-11-14 21:18:14 +0900

    検索メニューで日付を選択できるように機能追加

--- components/Event/EventSearchBar.vue
+++ components/Event/EventSearchBar.vue
@@ -1,28 +1,67 @@
 <template>
-  <v-text-field
-    v-model="searchKeyword"
-    rounded
-    solo
-    class="search-bar"
-    placeholder="イベントをさがす"
-    append-icon="mdi-magnify"
-    @click:append="searchEvents"
-    @keyup.enter="searchEvents"
-  ></v-text-field>
+  <v-menu
+    ref="menu"
+    v-model="menu"
+    :close-on-content-click="false"
+    class="search-menu"
+  >
+    <template #activator="{ on, attrs }">
+      <v-text-field
+        v-model="searchKeyword"
+        rounded
+        solo
+        class="search-bar"
+        placeholder="さがす"
+        append-icon="mdi-magnify"
+        v-bind="attrs"
+        v-on="on"
+      ></v-text-field>
+    </template>
+
+    <v-card>
+      <v-card-text>
+        <v-text-field
+          v-model="searchKeyword"
+          label="キーワード（例:お祭り、公園）"
+          solo
+          class="keyword-field"
+        ></v-text-field>
+        <div>日付を選択</div>
+        <v-date-picker
+          v-model="selectedDate"
+          locale="ja"
+          class="custom-date-picker"
+        ></v-date-picker>
+        <div class="text-center">
+          <v-btn @click="searchEvents">さがす</v-btn>
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-menu>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      searchKeyword: "", // ユーザーが入力した検索キーワード
+      searchKeyword: "",
+      selectedDate: null,
+      menu: false,
     };
   },
   methods: {
     searchEvents() {
-      // 親コンポーネントに検索キーワードを送信
-      this.$emit("search", this.searchKeyword);
-      console.log("検索キーワード:", this.searchKeyword); // TODO 本番環境では削除
+      this.$emit("search", {
+        keyword: this.searchKeyword,
+        date: this.selectedDate,
+      });
+      this.menu = false;
+      console.log(
+        "検索キーワード:",
+        this.searchKeyword,
+        "選択日付:",
+        this.selectedDate
+      );
     },
   },
 };
