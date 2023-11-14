90dd1adf75e2a2348c2922afaf36d73ca51012f4
shtan14 <wrzn.inw.snun@gmail.com>
2023-11-14 19:02:38 +0900

    ログイン後no

--- components/LoggedIn/LoggedInAppBar.vue
+++ components/LoggedIn/LoggedInAppBar.vue
@@ -1,20 +1,49 @@
 <template>
   <v-app-bar app :height="homeAppBarHeight" elevation="0" color="white">
-    <nuxt-link to="/" class="text-decoration-none" @click.native="clearSearch">
-      <app-logo />
-    </nuxt-link>
+    <v-container fluid>
+      <v-row no-gutters align="center" justify="center">
+        <v-col cols="3">
+          <v-row no-gutters align="center">
+            <v-col cols="auto">
+              <nuxt-link
+                to="/"
+                class="text-decoration-none"
+                @click.native="clearSearch"
+              >
+                <app-logo />
+              </nuxt-link>
+            </v-col>
+            <v-col cols="auto">
+              <nuxt-link
+                to="/"
+                class="text-decoration-none"
+                @click.native="clearSearch"
+              >
+                <app-title
+                  class="hidden-mobile-and-down primary--text font-weight-bold"
+                />
+              </nuxt-link>
+            </v-col>
+          </v-row>
+        </v-col>
 
-    <nuxt-link to="/" class="text-decoration-none" @click.native="clearSearch">
-      <app-title
-        class="hidden-mobile-and-down primary--text font-weight-bold"
-      />
-    </nuxt-link>
-    <v-spacer />
+        <v-col cols="6">
+          <v-row justify="center">
+            <v-col cols="auto">
+              <event-search-bar v-if="showSearchBar" @search="handleSearch" />
+            </v-col>
+          </v-row>
+        </v-col>
 
-    <event-search-bar v-if="showSearchBar" @search="handleSearch" />
-    <v-spacer />
-
-    <logged-in-app-bar-account-menu />
+        <v-col cols="3">
+          <v-row justify="end">
+            <v-col cols="auto">
+              <logged-in-app-bar-account-menu />
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app-bar>
 </template>
 
